72f751eea6e4c9d731cf24c03de96f78e9f14e10
kinukage <76907246+kinukage@users.noreply.github.com>
2021-02-24 08:27:11 +0900

    課題提出
    フォームに入力した内容を取得の作成

---
+++ 2-11/index.php
@@ -0,0 +1,19 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス<input type="text" name="email" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+<?php
+echo '<br>';
+echo "1. 仕様書・設計書"."<br>";
+echo "やり方や、その順序を記した文書・システムに関する構造、形、機能などを定義したもの"."<br>";
+echo "2. Git"."<br>";
+echo "プログラムのソースコードなどの変更履歴を記録・追跡するための分散型バージョン管理システム"."<br>";
+echo "3. マージツール"."<br>";
+echo "MDLファイル中のマップ式をテキスト形式で記述したファイル「マップ式ファイル」に出力する機能
+"."<br>";
+?>
