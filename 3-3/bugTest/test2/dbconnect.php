be7dd12e65b9a953a1f62f0183b1c08aa9623290
kinukage <76907246+kinukage@users.noreply.github.com>
2021-03-24 23:49:55 +0900

    第３章課題提出
    バグ修正を行うを作成

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "yigroupBlog";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE";
